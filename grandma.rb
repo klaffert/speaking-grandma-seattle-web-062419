5d7e9857e0824c80171227eb226c8d33e0c32f01
Kelliann Lafferty <kell10192@gmail.com>
2019-06-04 18:36:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,8 @@
 # Write a speak_to_grandma method.
 
-def speak_to_grandma(phrase = "")
+phrase = "I love you Grandma!"
+
+def speak_to_grandma(phrase)
   if phrase == "I love you Grandma!".upcase
   "I LOVE YOU TOO PUMPKIN!"
 elsif phrase == "Hi, Nana, how are you?".downcase
